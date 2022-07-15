5f91ad619ad3572b376e7a653c80ad80a9dc71b7
felipegueller <felipe.gueller@gmail.com>
2022-07-15 09:53:53 -0300

    :tada: feat: creating the product-details component

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor () {}
+
+  ngOnInit(): void {}
+}
