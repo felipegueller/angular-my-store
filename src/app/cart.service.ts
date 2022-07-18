6b98897d64c103de3798a7de19428a3fe114ffe9
felipegueller <felipe.gueller@gmail.com>
2022-07-18 09:58:59 -0300

    :sparkles: feat: creating the getShippingCart method

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
-import { Product } from './products';
+import { Product } from './products'
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +9,24 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = []
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  addToCart (product: Product):void {
+  addToCart(product: Product): void {
     this.items.push(product)
   }
 
-  getItems (): Product[] {
-    return this.items;
+  getItems(): Product[] {
+    return this.items
   }
 
-  clearCart (): Product[] {
-    this.items = [];
-    return this.items;
+  clearCart(): Product[] {
+    this.items = []
+    return this.items
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      'assets/shipping.json'
+    )
   }
 }
